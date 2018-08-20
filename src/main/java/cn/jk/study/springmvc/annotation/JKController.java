4f84cf721051db75cc5b2e7e0e2b77437b5a81f3
jiakang <pengjiakang@champion-ic.com>
2018-08-20 18:07:42 +0800

    20180820.1

--- src/main/java/cn/jk/study/springmvc/annotation/JKController.java
+++
@@ -1,7 +0,0 @@
-package cn.jk.study.springmvc.annotation;
-
-/**
- * Created by jiakang on 2018/8/20.
- */
-public class JKController {
-}
---
+++ src/main/java/cn/jk/study/springmvc/servlet/MyDispatcherServlet.java
@@ -0,0 +1,29 @@
+package cn.jk.study.springmvc.servlet;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by jiakang on 2018/8/20.
+ */
+public class MyDispatcherServlet extends HttpServlet {
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
